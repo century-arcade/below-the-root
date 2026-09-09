b93482734dff62789ce00a7f900f859a5dadb123
Saul Pwanson <code@saul.pw>
2026-09-09 09:53:49 -0700

    change spacing of status line

--- src/status.js
+++ src/status.js
@@ -9,7 +9,7 @@ export function statusRows(state) {
   const spirit = `SPIRIT ${p.spiritEnergy}/${p.spiritLimit}`;
   return [
     place(top, PANEL_COLS - p.name.length, p.name),
-    place(place(place(place('', 0, `STAMINA ${p.stamina}`), 11, `FOOD ${p.food}`), 19, `REST ${p.rest}`),
+    place(place(place(place('', 0, `STAMINA ${p.stamina}`), 12, `FOOD ${p.food}`), 21, `REST ${p.rest}`),
       PANEL_COLS - spirit.length, spirit),
   ];
 }
